e3c9501dd46444b718cf675f2e004c3e8d61d718
brian.chan <brian.chan@rakuten.com>
2019-03-07 16:55:50 +0900

    initial commit for connecting github

---
+++ MovieViewer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MovieViewer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
