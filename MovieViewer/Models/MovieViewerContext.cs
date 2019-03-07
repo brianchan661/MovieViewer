b1fdf0f1a76f2d3948a52718139a595d6e3a8790
brian.chan <brian.chan@rakuten.com>
2019-03-08 00:04:24 +0900

    add base pages and local db connection

---
+++ MovieViewer/Models/MovieViewerContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieViewer.Models
+{
+    public class MovieViewerContext : DbContext
+    {
+        public MovieViewerContext(DbContextOptions<MovieViewerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MovieViewer.Models.Movie> Movie { get; set; }
+    }
+}
--- MovieViewer/Startup.cs
+++ MovieViewer/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MovieViewer.Models;
 
 namespace MovieViewer
 {
@@ -32,7 +34,14 @@ namespace MovieViewer
             });
 
 
+            services.AddDbContext<MovieViewerContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("MovieContext")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //services.AddDbContext<MovieViewerContext>(options =>
+                //options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
